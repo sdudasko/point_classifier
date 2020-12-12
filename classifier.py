c021bc8ad4ab47f8e2c410524e35658039ce13e9
stefan.duadsko <stefan.dudasko@triad.sk>
2020-12-12 16:32:37 +0100

    spravne suradnice pre testovacie body

--- classifier.py
+++ classifier.py
@@ -11,64 +11,68 @@ COLORS_SHORT = ("R", "G", "B", "P")
 class classifier:
     STATISTICS = []
     testing_groups = {}
+    distances = []
 
-    def __init__(self, STATISTICS):
+    def __init__(self, STATISTICS, init_training_points = True):
         self.STATISTICS = STATISTICS
 
-        self.testing_groups = {
-            'R': [(-45, -44), (-41, -30), (-18, -24), (-25, -34), (-20, -14)],
-            'G': [(45, -44), (41, -30), (18, -24), (25, -34), (20, -14)],
-            'B': [(-45, 44), (-41, 30), (-18, 24), (-25, 34), (-20, 14)],
-            'P': [(45, 44), (41, 30), (18, 24), (25, 34), (20, 14)],
-        }
+        if init_training_points:
+            self.testing_groups = {
+                'R': [(-4500, -4400), (-4100, -3000), (-1800, -2400), (-2500, -3400), (-2000, -1400)],
+                'G': [(4500, -4400), (4100, -3000), (1800, -2400), (2500, -3400), (2000, -1400)],
+                'B': [(-4500, 4400), (-4100, 3000), (-1800, 2400), (-2500, 3400), (-2000, 1400)],
+                'P': [(4500, 4400), (4100, 3000), (1800, 2400), (2500, 3400), (2000, 1400)],
+            }
+            # Adding training points to map
+            x_es = []
+            y_es = []
 
-        x_es = []
-        y_es = []
+            for red_point in self.testing_groups['R']:
+                x_es.append(red_point[0])
+                y_es.append(red_point[1])
 
-        for red_point in self.testing_groups['R']:
-            x_es.append(red_point[0])
-            y_es.append(red_point[1])
+            plt.plot(x_es, y_es, 'ro', color='red')
 
-        plt.plot(x_es, y_es, 'ro', color='red')
+            x_es.clear()
+            y_es.clear()
 
-        x_es.clear()
-        y_es.clear()
+            for green_point in self.testing_groups['G']:
+                x_es.append(green_point[0])
+                y_es.append(green_point[1])
+            plt.plot(x_es, y_es, 'ro', color='green')
+            x_es.clear()
+            y_es.clear()
 
-        for green_point in self.testing_groups['G']:
-            x_es.append(green_point[0])
-            y_es.append(green_point[1])
-        plt.plot(x_es, y_es, 'ro', color='green')
-        x_es.clear()
-        y_es.clear()
+            for blue_point in self.testing_groups['B']:
+                x_es.append(blue_point[0])
+                y_es.append(blue_point[1])
+            plt.plot(x_es, y_es, 'ro', color='blue')
+            x_es.clear()
+            y_es.clear()
 
-        for blue_point in self.testing_groups['B']:
-            x_es.append(blue_point[0])
-            y_es.append(blue_point[1])
-        plt.plot(x_es, y_es, 'ro', color='blue')
-        x_es.clear()
-        y_es.clear()
-
-        for purple_point in self.testing_groups['P']:
-            x_es.append(purple_point[0])
-            y_es.append(purple_point[1])
-        plt.plot(x_es, y_es, 'ro', color='purple')
+            for purple_point in self.testing_groups['P']:
+                x_es.append(purple_point[0])
+                y_es.append(purple_point[1])
+            plt.plot(x_es, y_es, 'ro', color='purple')
 
 
-    def get_classification(self, p, k):
-        distances = []
-        for group in self.testing_groups:
-            for feature in self.testing_groups[group]:
-                euclidean_distances = math.sqrt((feature[0] - p[0]) ** 2 + (feature[1] - p[1]) ** 2)
-                distances.append((euclidean_distances, group))
+    def get_classification(self, p, k, use_distances = False):
 
-        distances = sorted(distances)[:k]
+        if not use_distances:
+            distances = []
+            for group in self.testing_groups:
+                for feature in self.testing_groups[group]:
+                    pythagoras_formula = math.sqrt((feature[0] - p[0]) ** 2 + (feature[1] - p[1]) ** 2)
+                    distances.append((pythagoras_formula, group))
 
-        freqRed = 0  # R
-        freqGreen = 0  # G
-        freqBlue = 0  # B
-        freqPurple = 0  # P
+            self.distances = sorted(distances)[:k]
 
-        for distance in distances:
+        freqRed = 0
+        freqGreen = 0
+        freqBlue = 0
+        freqPurple = 0
+
+        for distance in self.distances:
             if distance[1] == "R":
                 freqRed += 1
             elif distance[1] == "G":
@@ -88,12 +92,12 @@ class classifier:
     def create_and_classify_red_point(self, k=3):
 
         if randrange(100) >= config.CHANCE_FOR_RANDOM_POINT:
-            x = random.randint(-config.GENERATOR_RANGE, 5)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, 5)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, 500)
+            y = random.randint(-config.GENERATOR_RANGE, 500)
             p = (x, y)
         else:
-            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
+            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
             p = (x, y)
 
         classification_index = self.get_classification(p, k)
@@ -106,12 +110,12 @@ class classifier:
     def create_and_classify_green_point(self, k=3):
 
         if randrange(100) >= config.CHANCE_FOR_RANDOM_POINT:
-            x = random.randint(-5, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, 5)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-500, config.GENERATOR_RANGE)
+            y = random.randint(-config.GENERATOR_RANGE, 500)
             p = (x, y)
         else:
-            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
+            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
             p = (x, y)
 
         classification_index = self.get_classification(p, k)
@@ -124,12 +128,12 @@ class classifier:
     def create_and_classify_blue_point(self, k=3):
 
         if randrange(100) >= config.CHANCE_FOR_RANDOM_POINT:
-            x = random.randint(-config.GENERATOR_RANGE, 5)  # potom upravit o 2 desatinne miesta
-            y = random.randint(5, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, 500)
+            y = random.randint(500, config.GENERATOR_RANGE)
             p = (x, y)
         else:
-            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
+            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
             p = (x, y)
 
         classification_index = self.get_classification(p, k)
@@ -142,12 +146,12 @@ class classifier:
     def create_and_classify_purple_point(self, k=3):
 
         if randrange(100) >= config.CHANCE_FOR_RANDOM_POINT:
-            x = random.randint(-5, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-5, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-500, config.GENERATOR_RANGE)
+            y = random.randint(-500, config.GENERATOR_RANGE)
             p = (x, y)
         else:
-            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
-            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)  # potom upravit o 2 desatinne miesta
+            x = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
+            y = random.randint(-config.GENERATOR_RANGE, config.GENERATOR_RANGE)
             p = (x, y)
 
         classification_index = self.get_classification(p, k)
@@ -157,7 +161,10 @@ class classifier:
         # For statistics, we either increment by one if right our classification matches or zero
         self.STATISTICS[k] += (COLORS_SHORT[classification_index] == "P")
 
-    def classify_random(self, p, k):
-        classification_index = self.get_classification(p, k)
-        self.testing_groups[COLORS_SHORT[classification_index]].append(p)  # Pridavame to pola trenovacich
+    def classify_random(self, p, k, first_iteration = False):
+        if first_iteration:
+            classification_index = self.get_classification(p, k, False)
+        else:
+            classification_index = self.get_classification(p, k, True)
+
         plt.plot(p[0], p[1], 'or', color=COLORS[classification_index])  # Pridame bod a dame ho do klasifikacie
--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-from array import *
-import matplotlib.pyplot as plt
-import math
 import random
-from classifier import classifier
+import matplotlib.pyplot as plt
+
 import config
+from classifier import classifier
 
 STATISTICS = {
     1: 0,
-    3: 0,
-    7: 0,
-    15: 0
+    3: 1,
+    7: 1,
+    15: 1
 }
 
 _classifier = classifier(STATISTICS)
-
-def get_class_where_i_belong():
-    pass
-
-
 all_points = list()
 
 for group in classifier.testing_groups:
@@ -28,29 +22,37 @@ for group in classifier.testing_groups:
 def get_missing_points():
     matrix = list()
     for i in range(-config.GENERATOR_RANGE, config.GENERATOR_RANGE):
+        print(f"Getovanie radu: {i}")
         for j in range(-config.GENERATOR_RANGE, config.GENERATOR_RANGE):
             matrix.append([i, j])
     random.shuffle(matrix)
 
     return matrix
 
-
+first_iteration = True
 def fill_missing_points():
+    global first_iteration
     pts = get_missing_points()
+
+    _classifier = classifier(STATISTICS, False)
+
     for i in range(-2 * config.GENERATOR_RANGE ** 2, 2 * config.GENERATOR_RANGE ** 2):
         p = (pts[i][0], pts[i][1])
-
-        _classifier = classifier(STATISTICS)
-        _classifier.classify_random(p, 1)
+        print(f"Vyplnanie radu: {i}")
+        if first_iteration:
+            _classifier.classify_random(p, 1, True)
+            first_iteration = False
+        else:
+            _classifier.classify_random(p, 1, False)
 
 
 def fill_new_training_points():
-    # for k in [1, 3, 7, 15]:
-    for k in [3]:
+    for k in [1, 3, 7, 15]:
         plt.plot([-config.GENERATOR_RANGE, config.GENERATOR_RANGE], [0, 0])
         plt.plot([-0, 0], [-config.GENERATOR_RANGE, config.GENERATOR_RANGE])
         plt.title(f'k = {k}')
 
+        _classifier = classifier(STATISTICS)
         for x in range(config.NEW_POINTS_KVARTAL_COUNT):
             _classifier.create_and_classify_red_point(k)
             _classifier.create_and_classify_green_point(k)
@@ -60,17 +62,13 @@ def fill_new_training_points():
 
 
 fill_new_training_points()
-fill_missing_points()
-plt.show()
+# fill_missing_points()
+# plt.show()
 
 def prettify_statistics():
+    print(f"Number of points: {config.NEW_POINTS_KVARTAL_COUNT * 4 + 20}")
     for k_val in STATISTICS:
-        if k_val == 3:
-            print(f"{_classifier.STATISTICS[k_val]} / {_classifier.STATISTICS[k_val] / (config.NEW_POINTS_KVARTAL_COUNT * 4)}")
-            print(STATISTICS[k_val] / (config.NEW_POINTS_KVARTAL_COUNT * 4) * 100)
+        print(f"k = {k_val}. Classified correctly: {STATISTICS[k_val] / (config.NEW_POINTS_KVARTAL_COUNT * 4) * 100}%")
 
 
 prettify_statistics()
-
-# plt.show()
-# plt.axis([-15, 15, -15, 15])
